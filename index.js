95384880accfa18f15714c57d8af19b1547eb2da
sohel0199 <sohel019954@gmail.com>
2022-12-05 19:21:21 +0600

    all allcourses data is loaded

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
 app.use(cors());
 
 const courses = require('./data/courses.json')
+const allcourses = require('./data/allcourses.json')
 
 
 app.get('/', (req, res) => {
     res.send('This leaning platform')
 });
-app.get('/courses', (rep, res) => {
+
+app.get('/courses', (req, res) => {
     res.send(courses)
+})
+app.get('/allcourses', (req, res) => {
+    res.send(allcourses)
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedcourse = allcourses.find(c => c.id == id)
+    res.send(selectedcourse);
 })
 
 app.listen(port, () => {
